82e385f64398e56b694d2a26d3bc874834eec5dc
Mootassam <mootassame@gmail.com>
2023-08-29 20:47:09 +0100

    add the new task

--- src/component/red/Red.tsx
+++ src/component/red/Red.tsx
@@ -8,8 +8,37 @@ function Red(props) {
         <div className="detail__header"> Detalhe do Lançamento</div>
         <div></div>
       </div>
-      <div className="red__subheader"></div>
-      im the best in the world Red
+      <div className="red__subheader">
+        <div className="subheader__top">
+          <label htmlFor="" className="title__red">
+            Pix recebido
+          </label>
+          <label htmlFor="" className="description__red">
+            Cash pay meios de pagamen
+          </label>
+        </div>
+        <div className="subheader__bottom">
+          <label htmlFor="" className="title__red">
+            Pix recebido
+          </label>
+          <label htmlFor="" className="description__red">
+            Cash pay meios de pagamen
+          </label>
+        </div>
+
+      </div>
+      <div className="red__content">
+        <div className="content__redtop">
+          <span className="content__title">
+            Saldo em conta após este lançamento
+          </span>
+          <span className="content__amount">lasR$ 495,00</span>
+        </div>
+        <div className="content__redbottom">
+          <span className="content__title">Data</span>
+          <span className="content__amount">31/07/2023</span>
+        </div>
+      </div>
     </div>
   );
 }
